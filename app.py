96cbf0997af7d3cf6ea0046cc6b74dd6f82e7df1
Aareet Shermon <aareet@shermon.com>
2013-08-04 12:12:07 -0400

    Added hajku save support

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pymongo
 import os
 from bson import ObjectId
+import json
+import urllib
+from datetime import datetime
+
 app = Flask(__name__)
 
-db = pymongo.MongoClient(os.getenv('MONGOHQ_URL'))
+db = pymongo.MongoClient(os.getenv('MONGOHQ_URL')).hajkus
 
 @app.route('/', methods=["GET"])
 def write_hajku():
     return render_template("index.html")
 
+
 @app.route('/submit', methods=["POST"])
 def submit_hajku():
-    hajku = []
     if request.method == "POST":
-         hajku["line1"] = request.form["line1"]
-         hajku["line2"] = request.form["line2"]
-         hajku["line3"] = request.form["line3"]
+        hajku = {}
+        hajku["line1"] = request.form["line1"]
+        hajku["line2"] = request.form["line2"]
+        hajku["line3"] = request.form["line3"]
+        hajku["created_on"] = datetime.utcnow()
+        
+        objectid = db.haikus.save(hajku)
+        print str(objectid)
 
-         respo = db.hajkus.save(hajku)
-         return respo
+        return redirect("/view?hajku=%s" % str(objectid))
 
 @app.route('/view', methods=["GET"])
 def view_hajku():
     objid = request.args.get("hajku")
- 
 
+    return objid
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
