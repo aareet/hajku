924fae1260fcf54096f11d1f5d2371146112f596
Aareet Shermon <aareet@shermon.com>
2013-08-04 11:39:19 -0400

    cleaned up

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pymongo
+import os
+from bson import ObjectId
 app = Flask(__name__)
 
+db = pymongo.MongoClient(os.getenv('MONGOHQ_URL'))
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=["GET"])
+def write_hajku():
+    return render_template("index.html")
+
+@app.route('/submit', methods=["POST"])
+def submit_hajku():
+    hajku = []
+    if request.method == "POST":
+         hajku["line1"] = request.form["line1"]
+         hajku["line2"] = request.form["line2"]
+         hajku["line3"] = request.form["line3"]
+
+         respo = db.hajkus.save(hajku)
+         return respo
+
+@app.route('/view', methods=["GET"])
+def view_hajku():
+    objid = request.args.get("hajku")
+ 
 
 
 if __name__ == '__main__':
